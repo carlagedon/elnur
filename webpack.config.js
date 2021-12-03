d36691f1f6a7636aa97d93f59b45cbecbf0d1193
carlagedon <91982417+carlagedon@users.noreply.github.com>
2021-12-03 21:39:38 +0600

    main

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    entry: './src/app.js'
+    entry: './src/app.js',
+    output:{
+        filename: 'bundle.[chunkhash].js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devServer: {
+        port: 3000
+    },
+    pligins:[
+        new HTMLPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
