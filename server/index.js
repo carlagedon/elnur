4840f43fa4528c58202d613a60a5398b164f9b15
carlagedon <91982417+carlagedon@users.noreply.github.com>
2021-12-23 22:51:30 +0600

    main

--- server/index.js
+++ server/index.js
@@ -6,14 +6,19 @@ const cors = require("cors");
 const fileUpload = require('express-fileupload')
 const router = require("./routes/index");
 const errorHandler = require("./meddleware/ErrorHandlingMiddleweare");
+const { request, response } = require("express");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({}))
 app.use("/api", router);
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: 'WORKING!!!'})
+// })
 
 // обратная ошибка, последний M  iddleweare
 app.use(errorHandler);
